720c2601cef01f8f3989e105f4f60afcd54fb9c3
Dimitrii Vinogradov <siteddv@gmail.com>
2021-04-24 02:20:44 +0600

    set view

--- js/my.js
+++ js/my.js
@@ -1,32 +1,18 @@
-// let location1 = Math.floor(Math.random() * 5);
-// let location2 = location1 + 1;
-// let location3 = location2 + 1;
+const view = {
 
-// let guess;
-// let hits = 0;
-// let guesses = 0;
+   displayMessage: function (msg) {
+      const messageArea = document.querySelector("#messageArea");
+      messageArea.innerHTML = msg;
+   },
 
-// let isSunk = false;
+   displayHit: function (location) {
+      const cell = document.querySelector(location);
+      cell.setAttribute("class", "hit");
+   },
 
-// while (!isSunk) {
-//    guess = prompt("Ты готов выстрелить? (введи цифру 0-6):");
-//    if (guess < 0 || guess > 6) {
-//       alert("Повтори ввод. Введи цифру от 0 до 6");
-//    } else {
-//       ++guesses;
-//       if (guess == location1 || guess == location2 || guess == location3) {
-//          ++hits;
-//          alert("Попал :)");
-//          if (hits === 3) {
-//             isSunk = true;
-//             alert("Ты победил в игре!");
-//          }
-//       } else {
-//          alert("Мимо :(");
-//       }
-//    }
-// }
-// let status = "Ты выстрелил " + guesses +
-//    " раз для того, чтобы попасть по кораблю.\n" +
-//    "Это показывает уровень твоей точности, как " + (3 / guesses);
-// alert(status);
+   displayMiss: function (location) {
+      const cell = document.querySelector(location);
+      cell.setAttribute("class", "miss");
+   },
+
+}
