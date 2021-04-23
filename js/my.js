a1c9e3bd2576eb491b509ba099844d712f2abc50
Dimitrii Vinogradov <siteddv@gmail.com>
2021-04-23 18:48:05 +0600

    Made HTML and uploaded images

--- js/my.js
+++ js/my.js
@@ -1,32 +1,32 @@
-let location1 = Math.floor(Math.random() * 5);
-let location2 = location1 + 1;
-let location3 = location2 + 1;
+// let location1 = Math.floor(Math.random() * 5);
+// let location2 = location1 + 1;
+// let location3 = location2 + 1;
 
-let guess;
-let hits = 0;
-let guesses = 0;
+// let guess;
+// let hits = 0;
+// let guesses = 0;
 
-let isSunk = false;
+// let isSunk = false;
 
-while (!isSunk) {
-   guess = prompt("Ты готов выстрелить? (введи цифру 0-6):");
-   if (guess < 0 || guess > 6) {
-      alert("Повтори ввод. Введи цифру от 0 до 6");
-   } else {
-      ++guesses;
-      if (guess == location1 || guess == location2 || guess == location3) {
-         ++hits;
-         alert("Попал :)");
-         if (hits === 3) {
-            isSunk = true;
-            alert("Ты победил в игре!");
-         }
-      } else {
-         alert("Мимо :(" + guesses + " " + location1 + " " + location2 + " " + location3);
-      }
-   }
-}
-let status = "Ты выстрелил " + guesses +
-   " раз для того, чтобы попасть по кораблю.\n" +
-   "Это показывает уровень твоей точности, как " + (3 / guesses);
-alert(status);
+// while (!isSunk) {
+//    guess = prompt("Ты готов выстрелить? (введи цифру 0-6):");
+//    if (guess < 0 || guess > 6) {
+//       alert("Повтори ввод. Введи цифру от 0 до 6");
+//    } else {
+//       ++guesses;
+//       if (guess == location1 || guess == location2 || guess == location3) {
+//          ++hits;
+//          alert("Попал :)");
+//          if (hits === 3) {
+//             isSunk = true;
+//             alert("Ты победил в игре!");
+//          }
+//       } else {
+//          alert("Мимо :(");
+//       }
+//    }
+// }
+// let status = "Ты выстрелил " + guesses +
+//    " раз для того, чтобы попасть по кораблю.\n" +
+//    "Это показывает уровень твоей точности, как " + (3 / guesses);
+// alert(status);
