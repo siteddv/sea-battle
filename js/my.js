367e1e1914c4cad7aec862603b5aefca1411d5ab
Dimitrii Vinogradov <siteddv@gmail.com>
2021-04-24 02:49:01 +0600

    starter working with MVC

--- js/my.js
+++ js/my.js
@@ -16,3 +16,32 @@ const view = {
    },
 
 }
+
+
+
+const model = {
+   boardSize: 7,   // Размер игрового поля
+   numShips: 3,    // Количество кораблей в игре
+   shipLength: 3,  // Длина корабля в клетках
+   shipsSunk: 0,   // Количество потопленных кораблей
+
+   ships: [
+      ship1 = { location: ['10', '20', '30'], hits: ['', '', ''], },
+      ship2 = { location: ['32', '33', '34'], hits: ['', '', ''], },
+      ship3 = { location: ['63', '64', '65'], hits: ['', '', ''], },
+   ],
+
+   fire: function (coordinates) {
+      for (const shipName in this.ships) {
+         const ship = this.ships[shipName],
+         const location = ship[location];
+         const index = location.indexOf(coordinates);
+         if (index >= 0) {
+            ship.hits[index] = ' hit';
+            return true;
+         } else {
+            false;
+         }
+      }
+   }
+}
